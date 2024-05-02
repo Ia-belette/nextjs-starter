6307505d2f275ff9b33a0bff8201969f54bea0e4
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-05-02 20:41:14 +0200

    feat(auth): add google provider to next-auth configuration

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,8 +1,14 @@
 import { db } from '#/database/config';
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import NextAuth from 'next-auth';
+import Google from 'next-auth/providers/google';
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: DrizzleAdapter(db),
-  providers: [],
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
 });
